881e11629aa7b9077c3cd88bfaf4afb947833fcf
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-09 22:58:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
- # your code here!
-end
+def my_select(array) 
+  i =0 
+  select = [] 
+  while i < array.length 
+    if (yield(array[i]))
+      # select.push(array[i])OR 
+      select << array[i] 
+    end 
+    i += 1 
+  end 
+  select 
+end 
+
+my_select(nums) do |num| 
+  num.even? 
+end 
